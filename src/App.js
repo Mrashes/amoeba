74016130a69b4d89861a788956a85f999b0224b0
unknown <mrashes2@gmail.com>
2017-08-06 10:35:15 -0500

    added click counter

--- src/App.js
+++ src/App.js
@@ -2,48 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			currentItem: '',
-			username: '',
-			items: []
+
+var App = React.createClass({
+	getInitialState : function() {
+		return {
+			clicks: 0
 		}
-	}
-  
-  handleChange(e) {
+	},
 
-  }
+	addItem : function(e) {
+		e.preventDefault()
+		this.setState({
+			clicks: this.state.clicks + 1
+		})
+	},
 
-  handleSubmit(e) {
-
-	}
-
-	removeItem(itemId) {
-
-	}
-
-
-	componentDidMount() {
-
-	}
-
-	
 	render() {
 		return (
       <div>
         <div>Manage My Items</div>
         <form>
           <input type="input"/>
-          <input type="submit"/>
+          <input type="submit" onClick={this.addItem}/>
         </form>
         <div>My Items (0)</div>
+				<div>{this.state.clicks}</div>
       </div>
-      
-
 		);
 	}
-}
+})
+
 
 export default App;
