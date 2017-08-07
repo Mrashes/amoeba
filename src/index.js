b8bdfe3a982faf6a5faa08a84b1b50eb759c2255
unknown <mrashes2@gmail.com>
2017-08-07 09:18:18 -0500

    started importing new components to try and understand

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import Todo from './Todo';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <div><Welcome name="Sara" /><Todo/></div>;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
