809aa7da4efaf192699f65618bc1a01582c84a94
unknown <mrashes2@gmail.com>
2017-08-07 12:54:44 -0500

    modified tutorial to make it work

--- src/App.js
+++ src/App.js
@@ -1,50 +1,107 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Todo from './Todo'
-import ItemCreate from './ItemCreate.js'
+import firebase from './firebase.js'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			currentItem: '',
+			items: []
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+  
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
-  handleChange(event) {
-		this.setState({value: event.target.value});
+  handleSubmit = (e) => {
+	  e.preventDefault();
+	  const itemsRef = firebase.database().ref('items');
+	  const item = {
+	    title: this.state.currentItem,
+	  }
+	  itemsRef.push(item);
+	  this.setState({
+	    currentItem: ''
+	  });
 
-  }
+		// itemsRef.on('value', (snapshot) => {
+		//   console.log(snapshot.val());
+		// });
+	}
 
-  handleSubmit(event) {
-		// alert('A name was submitted: ' + this.state.value);
-			
-    event.preventDefault();
-  }
+	removeItem = (itemId) => {
+	  const itemRef = firebase.database().ref(`/items/${itemId}`);
+	  itemRef.remove();
+	}
 
-  render() {
-    return (
-			<div>
-        <ItemCreate/>
-        <Todo/> 
 
-          {/* Legacy code from fun freinds food to try understadning how to map? 
-        {this.state.items.map((item) => {
-          return (
-            <li key={item.id}>
-              <h3>{item.title}</h3>
-              <p>brought by: {item.user}</p>
-              <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-            </li>
-          )
-        })}         */}
+	componentDidMount = () => {
+	  const itemsRef = firebase.database().ref('items');
+	  itemsRef.on('value', (snapshot) => {
+	    let items = snapshot.val();
+	    let newState = [];
+	    for (let item in items) {
+	      newState.push({
+	        id: item,
+	        title: items[item].title,
+	      });
+	    }
+	    this.setState({
+	      items: newState
+	    });
+	  });
+	}
+
+	
+	render() {
+		return (
+			<div className='app'>
+				<header>
+						<div className='wrapper'>
+							<h1>Manage My Items</h1>
+						</div>
+				</header>
+				<div className='container'>
+					<section className='add-item'>
+							<form onSubmit={this.handleSubmit}>
+								<input type="text" name="currentItem" placeholder="What do you want to do?" onChange={this.handleChange} value={this.state.currentItem} />
+								<button>Add Item</button>
+							</form>
+					</section>
+					<div className='total'>
+						<p>My Items ({this.state.items.length})</p>
+					</div>
+					<section className='display-item'>
+						<div className='wrapper'>
+							<ul>
+							</ul>
+						</div>
+					</section>
+					<section className='display-item'>
+					  <div className="wrapper">
+					    <ul>
+					      {this.state.items.map((item) => {
+					        return (
+					          <li key={item.id}>
+					            <span>{item.title}</span>
+					            <button onClick={() => this.removeItem(item.id)}>X</button>
+					          </li>
+					        )
+					      })}
+					    </ul>
+					  </div>
+					</section>
+				</div>
 			</div>
-    );
-  }
+		);
+	}
 }
-
-
 
 export default App;
