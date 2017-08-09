23497f825ed3e866413905efe28035e44baf143f
unknown <mrashes2@gmail.com>
2017-08-09 12:03:44 -0500

    cleaning up the code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import './reset.css';
-// import firebase from './firebase.js'
 
 class App extends Component {
 	state = {
