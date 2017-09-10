8834704d021b115323c16a7233ee3a4ee8d05b3a
unknown <mrashes2@gmail.com>
2017-09-10 10:31:52 -0500

    react-create-app on heroku

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import Header from './header'
-import List from './list'
-import Input from './input'
+// import HeaderTodo from './header'
+// import List from './list'
+// import Input from './input'
 import './App.css';
-// import './reset.css';
+import './reset.css';
 
-function Store(initialState = {}) {
-	this.state = initialState;
-}
-Store.prototype.mergeState = function(partialState) {
-	Object.assign(this.state, partialState);
-};
+// function Store(initialState = {}) {
+// 	this.state = initialState;
+// }
+// Store.prototype.mergeState = function(partialState) {
+// 	Object.assign(this.state, partialState);
+// };
 
-var myStore = new Store();
+// var myStore = new Store();
 
 class Header extends Component {
   render () {
@@ -88,22 +88,22 @@ class App extends Component {
 			items: []
 		}
   
-  // handleChange = (e) => {
-  //   this.setState({
-  //     [e.target.name]: e.target.value
-  //   });
-  // }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-  // handleSubmit = (e) => {
-	//   e.preventDefault();
-	//   const item = {
-	//     title: this.state.currentItem,
-	//   }
-	//   this.state.items.push(item);
-	//   this.setState({
-	//     currentItem: ''
-	//   });
-	// }
+  handleSubmit = (e) => {
+	  e.preventDefault();
+	  const item = {
+	    title: this.state.currentItem,
+	  }
+	  this.state.items.push(item);
+	  this.setState({
+	    currentItem: ''
+	  });
+	}
 
 	removeItem = (item) => {
 		let array = this.state.items;
